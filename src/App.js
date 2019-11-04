285570d6be248642f592bf68e5ae43eb9c9abc99
jeln666 <je.luis.nascimento@hotmail.com>
2019-11-04 11:29:16 -0300

    finalização do desafio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/Header/Header';
+import Header from './components/Header';
+import PostList from './components/PostList';
 
 function App() {
   return (
-    <div className="container">
+    <>
       <Header />
-    </div>    
+      <PostList />
+    </>
   );
 }
 
